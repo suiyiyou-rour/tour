d2052e0b5715faf1cc5b1d0ab4140002fa20822e
huangshiqi <409222855@qq.com>
2018-01-18 22:17:14 +0800

    阿里短信新接口  -- 微信

--- Application/Weixin/Controller/SmsController.class.php
+++ Application/Weixin/Controller/SmsController.class.php
@@ -12,132 +12,123 @@ class SmsController extends Controller
     private $accessKeyId = '';
     // Access Access Key Secret
     private $accessKeySecret = '';
-    // 签名
-    private $signName = '';
-    // 模版ID
-    private $templateCode = '';
 
     public function __construct($cofig = array())
     {        
         $cofig = array(
             'accessKeyId' => 'LTAI4tfw5qzG499J',
             'accessKeySecret' => 'F3zIzdabHuUpVGH6XVBLg8EvUyWB89'
-            // 'signName' => '随意游网络',
-            // 'templateCode' => 'SMS_109705432'
         );
        
         // 配置参数
         $this->accessKeyId = $cofig ['accessKeyId'];
         $this->accessKeySecret = $cofig ['accessKeySecret'];
-        // $this->signName = $cofig ['signName'];
-        // $this->templateCode = $cofig ['templateCode'];
     }
 
-    private function percentEncode($string)
-    {
-        $string = urlencode($string);
-        $string = preg_replace('/\+/', '%20', $string);
-        $string = preg_replace('/\*/', '%2A', $string);
-        $string = preg_replace('/%7E/', '~', $string);
-        return $string;
-    }
-
-    /**
-     * 签名
+     /**
+     * 生成签名并发起请求
      *
-     * @param unknown $parameters
-     * @param unknown $accessKeySecret
-     * @return string
+     * @param $accessKeyId string AccessKeyId (https://ak-console.aliyun.com/)
+     * @param $accessKeySecret string AccessKeySecret
+     * @param $domain string API接口所在域名
+     * @param $params array API具体参数
+     * @param $security boolean 使用https
+     * @return bool|\stdClass 返回API接口调用结果，当发生错误时返回false
      */
-    public function computeSignature($parameters, $accessKeySecret)
-    {
-        ksort($parameters);
-        $canonicalizedQueryString = '';
-        foreach ($parameters as $key => $value) {
-            $canonicalizedQueryString .= '&' . $this->percentEncode($key) . '=' . $this->percentEncode($value);
+    private function request($params, $security=false) {
+        $apiParams = array_merge(array (
+            "SignatureMethod" => "HMAC-SHA1",
+            "SignatureNonce" => uniqid(mt_rand(0,0xffff), true),
+            "SignatureVersion" => "1.0",
+            "AccessKeyId" => 'LTAI4tfw5qzG499J',
+            "Timestamp" => gmdate("Y-m-d\TH:i:s\Z"),
+            "Format" => "JSON",
+        ), $params);
+        ksort($apiParams);
+        
+        $sortedQueryStringTmp = "";
+        foreach ($apiParams as $key => $value) {
+            $sortedQueryStringTmp .= "&" . $this->encode($key) . "=" . $this->encode($value);
         }
-        $stringToSign = 'GET&%2F&' . $this->percentencode(substr($canonicalizedQueryString, 1));
-        $signature = base64_encode(hash_hmac('sha1', $stringToSign, $accessKeySecret . '&', true));
-        return $signature;
-    }
 
-    /**
-     * 发送验证码 https://help.aliyun.com/document_detail/44364.html?spm=5176.doc44368.6.126.gSngXV
-     *
-     * @param unknown $mobile
-     * @param unknown $verify_code
-     *
-     */
-    public function sendVerify($mobile, $verify_code)
-    {
-        $params = array(
-            // 公共参数
-            'SignName' => $this->signName,
-            'Format' => 'JSON',
-            'Version' => '2017-05-25',
-            'AccessKeyId' => $this->accessKeyId,
-            'SignatureVersion' => '1.0',
-            'SignatureMethod' => 'HMAC-SHA1',
-            'SignatureNonce' => uniqid(),
-            'Timestamp' => gmdate('Y-m-d\TH:i:s\Z'),
-            // 接口参数
-            'Action' => 'SendSms',
-            'TemplateCode' => $this->templateCode,
-            'PhoneNumbers' => $mobile,
-            'TemplateParam' => $verify_code
-        );
-        // 计算签名并把签名结果加入请求参数
-        $params ['Signature'] = $this->computeSignature($params, $this->accessKeySecret);
-        // 发送请求
-        $url = 'https://dysmsapi.aliyuncs.com/?' . http_build_query($params);
+        $stringToSign = "GET&%2F&" . $this->encode(substr($sortedQueryStringTmp, 1));
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $result = json_decode($result, true);
-        if ($result['Code'] == 'OK') {
-            return true;
-        } else {
+        $sign = base64_encode(hash_hmac("sha1", $stringToSign, 'F3zIzdabHuUpVGH6XVBLg8EvUyWB89' . "&",true));
+
+        $signature = $this->encode($sign);
+
+        $url = ($security ? 'https' : 'http')."://dysmsapi.aliyuncs.com/?Signature={$signature}{$sortedQueryStringTmp}";
+
+        try {
+            $content = $this->fetchContent($url);
+            return json_decode($content);
+        } catch( \Exception $e) {
             return false;
         }
-
     }
 
-    /**
-     * 获取详细错误信息
-     *
-     * @param unknown $status
-     */
-    public function getErrorMessage($status)
+    private function encode($str)
     {
-        // https://api.alidayu.com/doc2/apiDetail?spm=a3142.7629140.1.19.SmdYoA&apiId=25450
-        $message = array(
-            'InvalidDayuStatus.Malformed' => '账户短信开通状态不正确',
-            'InvalidSignName.Malformed' => '短信签名不正确或签名状态不正确',
-            'InvalidTemplateCode.MalFormed' => '短信模板Code不正确或者模板状态不正确',
-            'InvalidRecNum.Malformed' => '目标手机号不正确，单次发送数量不能超过100',
-            'InvalidParamString.MalFormed' => '短信模板中变量不是json格式',
-            'InvalidParamStringTemplate.Malformed' => '短信模板中变量与模板内容不匹配',
-            'InvalidSendSms' => '触发业务流控',
-            'InvalidDayu.Malformed' => '变量不能是url，可以将变量固化在模板中'
-        );
-        if (isset ($message [$status])) {
-            return $message [$status];
-        }
-        return $status;
+        $res = urlencode($str);
+        $res = preg_replace("/\+/", "%20", $res);
+        $res = preg_replace("/\*/", "%2A", $res);
+        $res = preg_replace("/%7E/", "~", $res);
+        return $res;
     }
 
+    private function fetchContent($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "x-sdk-client" => "php/2.0.0"
+        ));
+
+        if(substr($url, 0,5) == 'https') {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
+
+        $rtn = curl_exec($ch);
+
+        if($rtn === false) {
+            trigger_error("[CURL_" . curl_errno($ch) . "]: " . curl_error($ch), E_USER_ERROR);
+        }
+        curl_close($ch);
+
+        return $rtn;
+    }
+
+    private function sendVerify($params){
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }else{
+            return false;
+        }
+        
+        // 请求短信发送接口
+        $content = $this->request(
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            ))
+        );
+        $content = json_decode(json_encode($content),TRUE);
+        if($content['Code'] == 'OK'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    
     //获取短信验证码 
     public function useSendSms()
     {
         $mobile = I('mobile');
-
-        // 手机号码验证
+        手机号码验证
         if(!is_phone($mobile)){
             $this->ajaxReturn(array('code' => 404, 'msg' => '手机号码不正确'));
         }
@@ -158,13 +149,17 @@ class SmsController extends Controller
         }                                                                                                          
 
         // 配置阿里云短信 -- 模板参数
-        $this->signName = '随意游网络';
-        $this->templateCode = 'SMS_109705432';
-        // 获取 $code
         $code = $this->generate_code();
-        // 请求接口
-        $params = '{"code":"' . $code . '"}';
-        $result = $this->sendVerify($mobile, $params);
+
+        $params["PhoneNumbers"] =  $mobile;
+        $params["SignName"] = "随意游网络";
+        $params["TemplateCode"] = "SMS_109705432";
+        $params['TemplateParam'] = Array (
+            "code" => $code
+        );
+        
+        $result = $this->sendVerify($params);
+
         if ($result) {
             $data['c_code']   =  $code;
             $data['c_mobile'] =  $mobile;
@@ -178,7 +173,6 @@ class SmsController extends Controller
         }
     }
 
-    
     /** 用户购买发送给供应商 短信验证码 
      *  @param mobile：供应商手机号
      *  @param productName：产品名
@@ -193,12 +187,16 @@ class SmsController extends Controller
         }
 
         // 配置阿里云短信 -- 模板参数
-        $this->signName = '随意游网络';
-        $this->templateCode = 'SMS_112470379';
-        
-        // 请求接口
-        $params = '{"productName":"' . $productName . '","userName":"' .$userName . '","time":"'.$time.'"}';
-        $result = $this->sendVerify($mobile, $params);
+        $params["PhoneNumbers"] =  $mobile;
+        $params["SignName"] = "随意游网络";
+        $params["TemplateCode"] = "SMS_112470379";
+        $params['TemplateParam'] = Array (
+            "productName" => $productName,
+            "userName" => $userName,
+            "time" => $time,
+        );
+
+        $result = $this->sendVerify($params);
         if($result){
             return 1;
         }else{
