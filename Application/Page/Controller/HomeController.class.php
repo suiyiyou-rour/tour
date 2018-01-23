f86ec2259d008555eafed7aaaaea0c58c2f56f64
lzl1993 <398212600@qq.com>
2018-01-23 11:12:37 +0800

    home/index

--- Application/Page/Controller/HomeController.class.php
+++ Application/Page/Controller/HomeController.class.php
@@ -5,9 +5,37 @@
 namespace Page\Controller;
 use Think\Controller;
 class HomeController extends Controller {
+    public function __construct(){
+        parent::__construct();
+        if(ACTION_NAME != "login" && ACTION_NAME !="index"){
+            if(empty(session('UserAdminLogin'))){
+                $this->display('home/index');
+            }
+            die;
+        }
+
+    }
+
     /*主页面显示*/
     public function index(){
         $this->display('home/index');
+    }
+
+    public function login(){
+        $user = I("post.user");
+        $pwd = I("post.pwd");
+        if(empty($user) || empty($pwd)){
+            $this->error('账号密码不能为空','index',1);
+        }
+        if($user != "syy" || $pwd !="suiyiyou123" ){
+            $this->error('账号密码不能为空','index',1);
+        }
+        echo "123";
+        exit;
+    }
+
+    public function kkk(){
+        echo 6;
     }
 
 
